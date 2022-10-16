4a54c43d723fa6430995876bfcf4f01df02733e0
cynthia nyame <cynthia.nyame@meltwater.org>
2022-10-16 15:27:26 +0000

    This contains the days of the week using switch

--- TryingMethods.java
+++ TryingMethods.java
@@ -1,9 +1,29 @@
 // This class contains my practice on java methods
 public class TryingMethods{
    public static void main(String[] args) {
-    
+        TryingMethods n = new TryingMethods();
+        System.out.println(n.dayOfTheWeek(6));
    }
-   
+   public String dayOfTheWeek(int weekDay) {
+    switch (weekDay) {
+        case 1:
+            return "Monday";
+        case 2:
+            return "Tuesday";
+        case 3:
+            return "Wednesday";
+        case 4:
+            return "Thursday";
+        case 5:
+            return "Friday";
+        case 6:
+            return "Saturday";
+        case 7:
+            return "Sunday";
+        default:
+            return "Not Available";
+    }
+   }
 }
 
 
